77d10145f59a2428af58ca6469c35c6f5c763e64
Fabio <dooxy76@gmail.com>
2025-05-04 01:09:44 +0200

    Fix CORS con configurazione semplice

--- server/index.js
+++ server/index.js
@@ -18,21 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 import cors from "cors";
-const allowedOrigins = [
-  "http://localhost:5173", // dev
-  "https://real-estate-server-3ciu32dxs-fabios-projects-71b0107c.vercel.app" // prod
-];
+import cors from "cors";
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://real-estate-server-3ciu32dxs-fabios-projects-71b0107c.vercel.app",
   credentials: true
 }));
+
 
 // Connect to the database
 main().catch((err) => console.log(err));
